2f694933c81d32dcc5f544eb677c40dc7dff5491
ofiszer <cc557639@gmail.com>
2024-07-22 14:57:34 +0200

    1st commit

---
+++ main.py
@@ -0,0 +1,48 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+import time
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake")
+screen.tracer()
+
+snake = Snake()
+food = Food()
+
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+# segment_1=Turtle("square")
+# segment_1.color("white")
+
+# segment_2=Turtle("square")
+# segment_2.color("white")
+# segment_2.goto(-20, 0)
+
+# segment_3=Turtle("square")
+# segment_3.color("white")
+# segment_3.goto(-40, 0)
+
+
+segments = []
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+
+
+screen.exitonclick()
