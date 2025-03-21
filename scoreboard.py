e08776a82c0abf6a8d063b34061a14ee7fc946c4
ofiszer <cc557639@gmail.com>
2025-03-21 14:01:40 +0100

    highscore
    added highscore reading and writing from/to txt file

--- scoreboard.py
+++ scoreboard.py
@@ -7,6 +7,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.highscore = int(data.read())
         self.color("yellow")
         self.penup()
         self.hideturtle()
@@ -15,13 +17,22 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} Highscore: {self.highscore}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        self.score = 0
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.highscore}")
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
         self.clear()
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    # def game_over(self):
+    #    self.goto(0, 0)
+    #    self.write("GAME OVER", align=ALIGNMENT, font=FONT)
--- snake.py
+++ snake.py
@@ -49,3 +49,10 @@ class Snake:
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
+
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)    # znikanie przegranch wężów
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
